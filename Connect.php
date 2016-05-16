408760c60495dd812f74bd990f746923051c1d7c
pingac127 <ping_ac127@hotmail.com>
2016-05-16 21:39:26 +0700

    add file php service connect

---
+++ Connect.php
@@ -0,0 +1,29 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF8");
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$databaseName = "Product-1";
+
+	/*
+	$host = "mysql.hostinger.co.uk";
+	$username = "u570291279_e2f";
+	$password = "cpe332kmutt";
+	$databaseName = "u570291279_e2f";
+
+	mysql_db_query($databaseName,"SET NAMES UTF8");
+
+	$con = mysqli_connect($host, $username, $password, $databaseName);
+
+	if (mysqli_connect_errno())
+	{
+		echo "false";
+	}
+	else
+	{
+		echo "true";
+	}
+	*/
+?>
